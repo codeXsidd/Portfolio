e07ee47881ba6f7ad036cc289b0f361e4b0c7f4e
codeXsidd <siddharth291206@gmail.com>
2026-08-15 22:04:33 +0530

    Fix Express 5 wildcard routing error

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const corsOptions = {
     optionsSuccessStatus: 204
 };
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.options('/{*splat}', cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
